14b5f8ba9f2802069c653e72c9a847ea6f1286de
radhika <boyallaradhika2013@gmail.com>
2025-03-25 19:43:46 +0530

    Strings

---
+++ src/String/palindrome.java
@@ -0,0 +1,38 @@
+package String;
+import java.util.Scanner;
+
+public class palindrome {
+    public static void main(String[] args) {
+            // madam -> madam, naman -> reveser(naman) ->
+
+            Scanner sc = new Scanner(System.in);
+            System.out.println("Enter the input, I will check Palindrome");
+            String userInput = sc.next();
+            String newString = reverseString(userInput);
+            if (newString.equalsIgnoreCase(userInput)) {
+                System.out.println("Palindrome");
+            } else {
+                System.out.println("Not Palindrome");
+
+            }
+
+
+        }
+
+        private static String reverseString(String userInput) {
+            String reversed = "";
+            for (int i = userInput.length() - 1; i >= 0; i--) {
+                reversed = reversed + userInput.charAt(i);
+            }
+
+            return reversed;
+        }
+
+        private static String reverseStringSB(String userInput) {
+            StringBuilder stringBuilder = new StringBuilder(userInput);
+            return stringBuilder.reverse().toString();
+        }
+
+
+    }
+
